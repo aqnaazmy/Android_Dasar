4d2b9aab54ce706f07f600be2472ff510815eaaa
aqnaazmy <aqnaazmy@gmail.com>
2023-07-25 21:01:17 +0700

    Android

---
+++ app/src/main/java/programmerzamannow/androiddasar/MainActivity.kt
@@ -0,0 +1,11 @@
+package programmerzamannow.androiddasar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.hello_world)
+    }
+}
